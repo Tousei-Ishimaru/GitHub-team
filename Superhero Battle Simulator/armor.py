50b2e75d4ad3855bf8d0cd341b18d49379967af4
AbhijayPaul <abhijaypaul@gmail.com>
2023-10-21 04:06:10 +0530

    Added block method

--- Superhero Battle Simulator/armor.py
+++ Superhero Battle Simulator/armor.py
@@ -1,5 +1,9 @@
 # armor.py
+import random
 class Armor:
     def __init__(self, name, max_block):
         self.name = name
         self.max_block = max_block
+    
+    def block(self):
+        return random.randint(0, self.max_block)
